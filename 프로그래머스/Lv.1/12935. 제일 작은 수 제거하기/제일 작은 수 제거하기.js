5544cefcbe824582f0d2f70156d67c9706a06162
leejieun2023 <154851474+leejieun2023@users.noreply.github.com>
2024-01-16 09:32:58 +0900

    [level Lv.1] Title: 제일 작은 수 제거하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    if (arr.length <= 1) {
+        return [-1];
+    }
+    
+    const min = Math.min(...arr);
+    const index = arr.indexOf(min);
+    
+    arr.splice(index, 1);
+    
+    return arr;
+}
