1f1cab3efb5e69f3122b1f7782c6d38ba1086351
이지은 <154851474+leejieun2023@users.noreply.github.com>
2024-03-01 18:13:26 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,6 +1,7 @@
 function solution(numer1, denom1, numer2, denom2) {
-    const denom = denom1 * denom2;
-    const numer = (numer1 * denom2) + (numer2 * denom1);
+    const denom = denom1 * denom2; // 분모 구하기
+    const numer = (numer1 * denom2) + (numer2 * denom1); // 분자 구하기
+    // -> 이렇게 해야 분자분모를 통분할 수 있음
     
     let minNumber;
     
@@ -9,7 +10,7 @@ function solution(numer1, denom1, numer2, denom2) {
     } else {
         minNumber = denom;
     }
-    // minNumber에 작은 숫자가 들어있음
+    // minNumber에 작은 숫자가 들어있음 -> 최대공약수를 찾아야함
     
     while (true) {
         if (numer % minNumber === 0) {
@@ -17,7 +18,7 @@ function solution(numer1, denom1, numer2, denom2) {
                 return [numer/minNumber ,denom/minNumber];
             }
         }
-        minNumber = minNumber - 1;
+        minNumber = minNumber - 1; // 만약 최대공약수가 아닐 때, 1을 뺌
     }
     
 }
